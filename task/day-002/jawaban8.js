bf22bce966599a0faf5e64271037ac681a7a7980
Andang Vijayanto Utomo <andang.utomo@xapiens.id>
2021-02-24 16:45:21 +0700

    revisi jawaban 8

--- task/day-002/jawaban8.js
+++ task/day-002/jawaban8.js
@@ -1,3 +1,9 @@
-for(let i = 7; i > -1; i--){
-    console.log(i)
-}
+let i = 7;
+const test = setInterval(() => {
+    if (i <= 0){
+        clearInterval(test);
+    }
+    console.log(i);
+    i--;
+    
+}, 1000)
