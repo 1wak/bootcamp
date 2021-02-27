a11efe2dd3a8ac690bdb05479690108e1a87a3b7
Andang Vijayanto Utomo <andang.utomo@xapiens.id>
2021-02-27 12:40:11 +0700

    mencoba beberapa cara penyelesaian

--- task/day-004/jawaban4.js
+++ task/day-004/jawaban4.js
@@ -1,34 +1,91 @@
-const { default: PQueue } = require("p-queue");
-const queue = new PQueue({ concurrency: 1 });
+// Hasil 1,2,3,Done tapi pakai library
+// Solved undefined
+// const { default: PQueue } = require("p-queue");
+// const queue = new PQueue({ concurrency: 1 });
 
-const myPromises = [
-  () =>
-    new Promise((resolve) => {
-      for (let i = 1; i <= 3; i++) {
-        setTimeout(() => {
-          resolve(i), console.log(i);
-        }, 1000);
-      }
-    }),
-  () =>
-    new Promise((resolve) => {
-      resolve("Done"), console.log("Done");
-    }),
-];
+// const myPromises = [
+//   () =>
+//     new Promise((resolve) => {
+//       for (let i = 1; i <= 3; i++) {
+//         setTimeout(() => {
+//           resolve(console.log(i));
+//         }, 1000);
+//       }
+//     }),
+//   () =>
+//     new Promise((resolve) => {
+//       resolve(console.log("Done"));
+//     }),
+// ];
 
-queue.addAll(myPromises).then(console.log);
-
-// const promise = new Promise((resolve) => {
-//   for (let i = 1; i <= 3; i++) {
-//     setTimeout(() => {
-//       resolve(i), console.log(i);
-//     }, 1000);
-//   }
+// queue.addAll(myPromises).then((res) => {
+//   console.log;
+//   return res;
 // });
 
-// promise
-//   .then((data) => {
-//     console.log(data);
+// Hasil 1,2,3,Undifined, Done
+// Solved undefined
+const promise = new Promise((resolve) => {
+  for (let i = 1; i <= 3; i++) {
+    setTimeout(() => {
+      resolve(console.log(i));
+    }, 1000);
+  }
+});
+
+promise
+  .then((data) => {
+    console.log;
+    return data;
+  })
+  .then(() => console.log("Done"))
+  .catch((err) => console.log(err));
+
+// Hasil 1,2,3,Done,NaN
+// Unsolved how to handle number to string
+// const count = () => {
+//   return new Promise((resolve) => {
+//     for (let i = 1; i <= 3; i++) {
+//       setTimeout(() => {
+//         resolve(console.log(i));
+//       }, 1000);
+//     }
+//   });
+// };
+
+// const codeDone = (cnt) => {
+//   return new Promise((resolve) => {
+//     resolve(cnt + console.log("Done"));
+//   });
+// };
+
+// count()
+//   .then((res) => {
+//     return codeDone(res);
 //   })
-//   .then(() => console.log("Done"))
+//   .then((res) => console.log(res))
 //   .catch((err) => console.log(err));
+
+// Promise.all
+// Unsolved
+// const count = () => {
+//   return new Promise((resolve) => {
+//     for (let i = 1; i <= 3; i++) {
+//       setTimeout(() => {
+//         resolve(console.log(i));
+//       }, 1000);
+//     }
+//   });
+// };
+
+// const codeDone = () => {
+//   return new Promise((resolve) => {
+//     resolve(console.log("Done"));
+//   });
+// };
+
+// Promise.all([count(), codeDone()]).then(([res1, res2]) => {
+//   // console.log(res1 + res2);
+//   console.log;
+//   return res1 + res2;
+// });
