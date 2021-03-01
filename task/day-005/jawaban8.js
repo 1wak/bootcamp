14c765e184139ce98c5dc8e5776541101137e8e5
Andang Vijayanto Utomo <andang.utomo@xapiens.id>
2021-03-01 13:57:56 +0700

    edit no 8

--- task/day-005/jawaban8.js
+++ task/day-005/jawaban8.js
@@ -1,24 +1,43 @@
 // Datatype Detector
+function isInt(n) {
+  return Number(n) === n && n % 1 === 0;
+}
+
+function isFloat(n) {
+  return Number(n) === n && n % 1 !== 0;
+}
+
+function isHex(n) {
+  return Boolean(String(n).match(/^0x[0-9a-f]+$/i));
+}
+
+function isOctal(n) {
+  return Boolean(String(n).match(/^0o[1-7][0-7]*$/g));
+}
+
+function isBinary(n) {
+  return Boolean(String(n).match(/^0b[0-1]{1,}$/g));
+}
 
 function dataType(x) {
-  if (typeof x === "string") {
+  if (isInt(x) === true) {
+    console.log("Integer");
+  } else if (isFloat(x) === true) {
+    console.log("Float");
+  } else if (isOctal(x) === true) {
+    console.log("Octal");
+  } else if (isHex(x) === true) {
+    console.log("Hexadecimal");
+  } else if (isBinary(x) === true) {
+    console.log("Binary");
+  } else if (typeof x === "string") {
     console.log("String");
   } else if (typeof x === []) {
     console.log("Array");
   } else if (typeof x === "object") {
     console.log("Object");
-  } else if (typeof x === parseInt()) {
-    console.log("Integer");
-  } else if (typeof x === parseFloat()) {
-    console.log("Float");
-  } else if (typeof x !== parseInt() && typeof x !== parseFloat()) {
-    console.log("Number");
-  } else if ((typeof x === x % 8) === 0) {
-    console.log("Octadecimal");
-  } else if (typeof x === "0x".concat(x)) {
-    console.log("Hexadecimal");
   } else {
-    console.log("Undefined");
+    console.log("Invalid");
   }
 }
 
@@ -33,6 +52,11 @@ dataType(1);
 dataType(1.25);
 dataType(16);
 dataType(8);
-dataType(0o42);
-dataType(0x1c);
-dataType(0b11);
+dataType("0o42");
+dataType("0x1c");
+dataType("0b11");
+dataType({ negara: "Indonesia" });
+dataType(39);
+dataType(44.5);
+dataType({});
+dataType(["lobak", "timun", "cabai"]);
