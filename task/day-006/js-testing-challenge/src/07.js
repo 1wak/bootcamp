bfae44737f418393bd84c5804dbae654e6d661a6
Andang Vijayanto Utomo <andang.utomo@xapiens.id>
2021-03-02 14:47:16 +0700

    update no 7

--- task/day-006/js-testing-challenge/src/07.js
+++ task/day-006/js-testing-challenge/src/07.js
@@ -1,1 +1,24 @@
-function repeatLetter(text, times) {}
+function repeatLetter(text, times) {
+  let store = text.split(" ");
+  let res = "";
+
+  for (let i = 0; i < store.length; i++) {
+    let word = store[i];
+    for (let j = 0; j < word.length; j++) {
+      for (let k = 0; k < times; k++) {
+        res += word[j];
+      }
+    }
+    res += " ";
+  }
+  let result = res.trim();
+  return result;
+}
+
+module.exports = repeatLetter;
+
+// console.log(repeatLetter("Hello World! ", 2));
+// console.log(repeatLetter("awioaj                ija jeoi j", 2));
+
+// var str = "     Hello World!     ";
+// console.log(str.trim());
