4264db26b3807f2e483bf0be653f147dfd944c00
Andang Vijayanto Utomo <andang.utomo@xapiens.id>
2021-03-10 23:04:17 +0700

    init day 12

--- task/day-011/reactandang/src/pages/Authentication/Login.js
+++ task/day-011/reactandang/src/pages/Authentication/Login.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import { Input, Submit } from "../../components";
+import { Submit } from "../../components";
 import { Field, Formik, Form } from "formik";
 import * as Yup from "yup";
 import { FaFacebookF, FaTwitter } from "react-icons/fa";
 
 export default class Login extends React.Component {
-  emailInput = "Kolom tidak boleh kosong";
-  passwordInput = "Kolom tidak boleh kosong";
+  emailInput = "Email tidak boleh kosong";
+  passwordInput = "Password tidak boleh kosong";
   passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
   errorValidation = () =>
     Yup.object().shape({
-      email: Yup.string().email(this.emailInput).required(),
+      email: Yup.string()
+        .email("Your email address is not valid please kindly check carefull")
+        .required(this.emailInput),
       password: Yup.string()
-        .matches(this.passwordRegex, { message: "Minimal 10" })
+        .matches(this.passwordRegex, {
+          message:
+            "Your password must be minimum 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character",
+        })
         .required(this.passwordInput),
     });
 
@@ -61,25 +66,23 @@ export default class Login extends React.Component {
                     className="input"
                     type="email"
                     placeholder="Your email address"
-                    required
                   />
-                  {errors.email && touched.email && <div>{errors.email}</div>}
+                  {errors.email && touched.email && (
+                    <div className="error__message">{errors.email}</div>
+                  )}
 
                   <Field
                     name="password"
                     className="input"
                     type="password"
                     placeholder="Your password"
-                    required
                   />
                   {errors.password && touched.password && (
-                    <div>{errors.password}</div>
+                    <div className="error__message">{errors.password}</div>
                   )}
 
                   <div className="login__div">
-                    <button type="submit" title="Login">
-                      Submit Bro
-                    </button>
+                    <Submit title="Login" />
                   </div>
                 </Form>
               )}
