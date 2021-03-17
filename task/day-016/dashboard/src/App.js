132d9c2f1694a5aaeb658848e103d55e1f2cdfea
Andang Vijayanto Utomo <andang.utomo@xapiens.id>
2021-03-17 13:59:33 +0700

    add context folder, component layout, data json

--- task/day-016/dashboard/src/App.js
+++ task/day-016/dashboard/src/App.js
@@ -1,11 +1,28 @@
+import { Button, Container, Input, Typography } from "@material-ui/core";
 import React, { Component } from "react";
+import Layout from "./components/Layout";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello todo</h1>
-      </div>
+      <Layout>
+        <Container className="app">
+          <Typography variant="h3" component="h1">
+            Todo APP
+          </Typography>
+          <Input type="text" />
+          <Button>Submit</Button>
+        </Container>
+      </Layout>
     );
   }
 }
+
+// create todo (form input)
+// mark as complete
+//     tandai kalau itu sudah komplit / terlaksana
+//     bisa pakai checkbox, berubah warna dll.
+// update
+// delete
+// data yang dipakai boleh pakai json atau dummy data
+// dibuat pakai context
