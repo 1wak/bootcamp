45e6d79450e81a31a09d7aeb3b5cdb1f5f811a06
Andang Vijayanto Utomo <andang.utomo@xapiens.id>
2021-03-17 09:11:29 +0700

    debug email regex masalah @ tervalidasi true

--- task/day-015/reactandang/src/utils/FormValidation/validationRules.js
+++ task/day-015/reactandang/src/utils/FormValidation/validationRules.js
@@ -3,7 +3,11 @@ export default function validateInfo(values) {
 
   if (!values.email) {
     errors.email = "Email required";
-  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+  } else if (
+    !/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(
+      values.email
+    )
+  ) {
     errors.email = "Email address is invalid";
   }
 
